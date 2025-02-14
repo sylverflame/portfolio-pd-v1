edc9ed5f9d1ded296cda43d0555ac021f6cccce7
Pratik D <tiptoppratik@gmail.com>
2025-02-14 15:46:22 +0000

    setup: tailwind css added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import "./App.css";
 
 function App() {
-  return <>App Component</>;
+  return (
+    <h1 className="text-3xl font-bold underline text-center">Hello world!</h1>
+  );
 }
 
 export default App;
